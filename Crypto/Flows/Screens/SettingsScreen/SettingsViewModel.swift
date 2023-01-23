8849739caab4b5a200342947083d5b6ef6bf35e8
vitalyaliskevych <98754902+vitalyaliskevych@users.noreply.github.com>
2023-01-23 16:10:46 +0200

    Crypto-14: Create SettingsScreen

---
+++ Crypto/Flows/Screens/SettingsScreen/SettingsViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  SettingsViewModel.swift
+//  Crypto
+//
+//  Created by Developer on 23.01.2023.
+//
+
+import Foundation
+
+
+class SettingsViewModel: ObservableObject {
+
+ @Published var isOn = false
+}
